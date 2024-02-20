bc7d48bc342ecc3c922d91e520fb8a8367de4066
shlokiyer123 <153591913+shlokiyer123@users.noreply.github.com>
2024-02-20 20:56:22 +0530

    Add files via upload

---
+++ JavaLabsrc/LAB6Packages/SEE/Externals.java
@@ -0,0 +1,48 @@
+package see;
+
+import cie.Internals;
+
+import java.util.Scanner;
+
+public class Externals extends Internals
+{
+
+    protected int marks[];
+    protected int finalMarks[];
+    public Externals() 
+    {
+
+        marks= new int[5];
+        finalMarks= new int[5];
+    }
+        
+    public void inputSEEmarks() 
+    {
+        
+        Scanner s = new Scanner(System.in);
+        
+        for(int i=0;i<5;i++) 
+        {
+        
+            System.out.print("Subject "+(i+1)+" marks: ");
+            marks[i] = s.nextInt();
+        } 
+    }
+    public void calculateFinalMarks() 
+    {
+
+        for(int i=0;i<5;i++)
+            finalMarks[i] = marks[i]/2 + super.marks[i];
+        
+    }
+        
+    public void displayFinalMarks() 
+    { 
+        displayStudentDetails();
+        for(int i=0;i<5;i++)
+            System.out.println("Subject " + (i+1) + ": " + finalMarks[i]);
+    
+    }
+        
+}
+
